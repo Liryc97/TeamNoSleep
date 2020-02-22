4035e6c1b38b2de70e182bab6e861549f6ab8f7e
Liryc97 <cyrilabesamis97@gmail.com>
2020-02-22 15:09:24 +0800

    initial

---
+++ TeamNoSleep/public/assets/ReadMe.txt
@@ -0,0 +1,10 @@
+Problems:
+1. putting header at bottom allows it to be banner, but slides 	links get in the way
+    - not banner?
+    - links problem?
+2. sliding only works on edge
+
+
+Remaining Steps:
+-Fill with Content
+-Full Design
--- TeamNoSleep/resources/views/welcome.blade.php
+++ TeamNoSleep/resources/views/welcome.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="assets/plugin/mdb/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/plugin/mdb/css/mdb.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    
 
         <!-- Fonts -->
+        <link rel="stylesheet" href="css/style.css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
+        {{-- <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -61,10 +67,10 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
-        </style>
+        </style> --}}
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        {{-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -95,6 +101,44 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+        </div> --}}
+{{--     
+            <div class="topnav">
+                <a class="active" href="#home">Home</a>
+                <a href="#contact"> <span>
+                <tr>
+                    <ul>
+                        <li></li></ul></tr>    
+                </span></a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+                <a href="#login_signup">Login/Sign up</a>
+                
+              </div> --}}
+              <section class="header">
+                <a href="one.html" class="h_link">Website </a>
+                <a href="two.html" class="h_link">Gallery </a>
+                <a href="three.html" class="h_link">About </a>
+                <font class="h_link">Contact Info </font>
+            </section>
+        
+            <p><br></p>
+            <section>
+                <h2 align="center">Contact Us</h2>
+                <br>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+                            <img src="assets/images/lalaWowee.png" width="100%"><br><center>Twitter</center></div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-12"><img src="assets/images/haha.jpg" width="100%"><br><center>Facebook</center></div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-12"><img src="assets/images/reknight.png" width="100%"><br><center>Landline</center></div>
+                    </div>
+                </div>
+            </section>
+              <div class="legal" style="position: fixed;">
+                blah blah blah
+                </div>
+              <script type="text/javascript" src="assets/plugin/mdb/js/bootstrap.min.js"></script>
+              <script type="text/javascript" src="assets/plugin/mdb/js/mdb.min.js"></script>
     </body>
 </html>
