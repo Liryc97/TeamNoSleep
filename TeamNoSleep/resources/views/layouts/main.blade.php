89aae307a2c9abe340cefc87744a09423d25bc7d
bishamon77722 <bishamon77721@gmail.com>
2020-02-22 16:53:38 +0800

    initial

--- TeamNoSleep/resources/views/layouts/main.blade.php
+++ TeamNoSleep/resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('/assets/plugin/mdb/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/plugin/mdb/css/mdb.css')}}">
 </head>
 <body>
 <header>
@@ -17,5 +19,11 @@
         
     </div>
 </header>
+
+@yield('content')
+
+<script src="{{asset('assets/plugin/mdb/js/jquery-3.4.1.min.js')}}"></script>
+<script src="{{asset('assets/plugin/mdb/js/bootstrap.js')}}"></script>
+<script src="{{asset('assets/plugin/mdb/js/mdb.js')}}"></script>
 </body>
 </html>
