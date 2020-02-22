59bc5899de82d4b9e87d5135be3194a818448644
Liryc97 <cyrilabesamis97@gmail.com>
2020-02-22 21:18:10 +0800

    text

---
+++ TeamNoSleep/resources/views/layouts/login.blade.php
@@ -0,0 +1,5 @@
+@extends('layous.welcome')
+
+<input type="text" placeholder="Email" name="email">
+<input type="text" placeholder="Password" name="password">
+<input type="text">
---
+++ TeamNoSleep/resources/views/layouts/users_landing.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>users login</title>
+</head>
+<body>
+    <label for="">id no:</label>
+    <input type="text" name="id" placeholder="">
+    <label for="">Name:</label>
+    <input type="text" name="name" placeholder="Change your Name">
+    <label for="">City:</label>
+    <input type="text" name="city" placeholder="City">
+    <label for="">Regions:</label>
+    <input type="text" name="regions" placeholder=" Change Region">
+    <label for="">Contact Number:</label>
+
+    <label for="">Contact Number:</label>
+    
+</body>
+</html>
